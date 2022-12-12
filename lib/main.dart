3c7f9d5c092c3a797d893cd398fbfecfb4996835
Kenta Inagaki <kenta.inagaki0107@gmail.com>
2022-12-12 21:17:07 +0900

    remove unnecessary pacakge

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:dutch_app/android_app.dart';
 import 'package:dutch_app/ios_app.dart';
 import 'package:flutter/material.dart';
-import 'package:device_info_plus/device_info_plus.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
