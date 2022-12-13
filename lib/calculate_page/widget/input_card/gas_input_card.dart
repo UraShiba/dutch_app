0490015c0e277a09f2fa99eaf17322e8e7bf4574
Kenta Inagaki <kenta.inagaki0107@gmail.com>
2022-12-13 21:52:19 +0900

    remove controller

--- lib/calculate_page/widget/input_card/gas_input_card.dart
+++ lib/calculate_page/widget/input_card/gas_input_card.dart
@@ -5,6 +5,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+class _GasFeeCalculatorData {
+  int? fuelConsumption;
+  int? startTrip;
+  int? endTrip;
+  int? litter;
+}
+
 class GasInputCard extends StatelessWidget {
   const GasInputCard({Key? key, required this.gasFee}) : super(key: key);
 
@@ -12,104 +19,119 @@ class GasInputCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _fuelConsumptionController = TextEditingController();
-    final _startConsumptionController = TextEditingController();
-    final _endConsumptionController = TextEditingController();
-    final _litter = TextEditingController();
-
+    final _gasFee = GlobalKey<FormState>();
+    var inputData = _GasFeeCalculatorData();
     return Card(
       color: const Color((0xFFF6F9FC)),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "Gas fee",
-              textAlign: TextAlign.left,
-              style: TextStyle(fontSize: 20),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text("Litter (km/l)"),
-                SizedBox(
-                  width: 50,
-                  height: 50,
-                  child: TextFormField(
-                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                    controller: _litter,
+        child: Form(
+          key: _gasFee,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Gas fee",
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 20),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("Litter (km/l)"),
+                  SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: TextFormField(
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      onSaved: (value) {
+                        if (value!.isNotEmpty) {
+                          inputData.litter = int.parse(value);
+                        }
+                      },
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text("Fuel Consumption (yen/l)"),
-                SizedBox(
-                  width: 50,
-                  height: 50,
-                  child: TextFormField(
-                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                    controller: _fuelConsumptionController,
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("Fuel Consumption (yen/l)"),
+                  SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: TextFormField(
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      onSaved: (value) {
+                        if (value!.isNotEmpty) {
+                          inputData.fuelConsumption = int.parse(value);
+                        }
+                      },
+                    ),
                   ),
-                ),
-              ],
-            ),
-            const Padding(padding: EdgeInsets.all(4)),
-            const Text("Travel distance"),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text("Start trip"),
-                SizedBox(
-                  width: 50,
-                  height: 50,
-                  child: TextFormField(
-                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                    controller: _startConsumptionController,
+                ],
+              ),
+              const Padding(padding: EdgeInsets.all(4)),
+              const Text("Travel distance"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("Start trip"),
+                  SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: TextFormField(
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      onSaved: (value) {
+                        if (value!.isNotEmpty) {
+                          inputData.startTrip = int.parse(value);
+                        }
+                      },
+                    ),
                   ),
-                ),
-                const Text("End trip"),
-                SizedBox(
-                  width: 50,
-                  height: 50,
-                  child: TextFormField(
-                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                    controller: _endConsumptionController,
+                  const Text("End trip"),
+                  SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: TextFormField(
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      onSaved: (value) {
+                        if (value!.isNotEmpty) {
+                          inputData.endTrip = int.parse(value);
+                        }
+                      },
+                    ),
                   ),
-                ),
-              ],
-            ),
-            const Padding(padding: EdgeInsets.all(8)),
-            Center(
-              child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black,
-                  ),
-                  onPressed: () {
-                    final totalAmount =
-                        context.read<TotalAmountCubit>().getTotalAmount();
+                ],
+              ),
+              const Padding(padding: EdgeInsets.all(8)),
+              Center(
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                    ),
+                    onPressed: () {
+                      _gasFee.currentState!.save();
+                      context.read<GasFeeCubit>().addGasFeeCalculatorData(
+                          GasFeeCalculatorData(
+                              fuelConsumption: inputData.fuelConsumption!,
+                              startTrip: inputData.startTrip!,
+                              endTrip: inputData.endTrip!,
+                              litter: inputData.litter!));
+                      final totalAmount =
+                          context.read<TotalAmountCubit>().getTotalAmount();
 
-                    context.read<GasFeeCubit>().addGasFeeCalculatorData(
-                        GasFeeCalculatorData(
-                            fuelConsumption:
-                                int.parse(_fuelConsumptionController.text),
-                            startTrip:
-                                int.parse(_startConsumptionController.text),
-                            endTrip: int.parse(_endConsumptionController.text),
-                            litter: int.parse(_litter.text)));
-                    context.read<GasFeeCubit>().calculateGasFee();
-                    context.read<TotalAmountCubit>().addFeeList(
-                        totalAmount.copyWith(
-                            sumGas: context.read<GasFeeCubit>().getGasFee()));
-                    context.read<TotalAmountCubit>().calculateTotalAmount();
-                  },
-                  child: const Text("Add gas fee")),
-            ),
-            gasFee
-          ],
+                      context.read<GasFeeCubit>().calculateGasFee();
+                      context.read<TotalAmountCubit>().addFeeList(
+                          totalAmount.copyWith(
+                              sumGas: context.read<GasFeeCubit>().getGasFee()));
+                      context.read<TotalAmountCubit>().calculateTotalAmount();
+                    },
+                    child: const Text("Add gas fee")),
+              ),
+              gasFee
+            ],
+          ),
         ),
       ),
     );
